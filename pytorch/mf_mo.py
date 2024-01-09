76d891be53d6dc2f88ab7ae5457724f57e1eef5a
Leona Hennig <l.hennig@ai.uni-hannover.de>
2024-01-09 11:37:31 +0100

    multi objective added

--- pytorch/mf_mo.py
+++ pytorch/mf_mo.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import warnings
+import time
 import argparse
 import torch
 import torch.nn as nn
@@ -15,10 +16,12 @@ from smac import HyperparameterOptimizationFacade, Scenario
 from smac.intensifier import Hyperband
 from smac.facade import MultiFidelityFacade as MFFacade
 from smac.facade import AbstractFacade
-from ConfigSpace import ConfigurationSpace, Categorical, Integer, Float
+from ConfigSpace import ConfigurationSpace, Configuration, Categorical, Integer, Float
 import multiprocessing as mp
 import matplotlib.pyplot as plt
+import numpy as np
 from typing import List
+from smac.multi_objective.parego import ParEGO
 
 if mp.get_start_method(allow_none=True) != 'spawn':
     mp.set_start_method('spawn')
@@ -167,6 +170,8 @@ def create_configspace():
     return cs
 
 def train_model(config, seed: int = 0, budget: int = 25):
+    start_time = time.time()
+
     model_config = {
         'type': config['model_type'],
         'optimizer': config['optimizer_type'],
@@ -192,7 +197,10 @@ def train_model(config, seed: int = 0, budget: int = 25):
     dsnn = DSNN(model_config, device)
     loss = dsnn.train(train_config)
 
-    return loss
+    return {
+            "loss": loss,
+            "time": time.time() - start_time,
+        }
 
 
 def plot_trajectory(facades: List[AbstractFacade]) -> None:
@@ -222,12 +230,49 @@ def plot_trajectory(facades: List[AbstractFacade]) -> None:
     plt.legend()
     plt.show()
 
+def plot_pareto(smac: AbstractFacade, incumbents: List[Configuration]) -> None:
+    """Plots configurations from SMAC and highlights the best configurations in a Pareto front."""
+    average_costs = []
+    average_pareto_costs = []
+    for config in smac.runhistory.get_configs():
+        # Since we use multiple seeds, we have to average them to get only one cost value pair for each configuration
+        average_cost = smac.runhistory.average_cost(config)
+
+        if config in incumbents:
+            average_pareto_costs += [average_cost]
+        else:
+            average_costs += [average_cost]
+
+    # Let's work with a numpy array
+    costs = np.vstack(average_costs)
+    pareto_costs = np.vstack(average_pareto_costs)
+    pareto_costs = pareto_costs[pareto_costs[:, 0].argsort()]  # Sort them
+
+    costs_x, costs_y = costs[:, 0], costs[:, 1]
+    pareto_costs_x, pareto_costs_y = pareto_costs[:, 0], pareto_costs[:, 1]
+
+    plt.scatter(costs_x, costs_y, marker="x", label="Configuration")
+    plt.scatter(pareto_costs_x, pareto_costs_y, marker="x", c="r", label="Incumbent")
+    plt.step(
+        [pareto_costs_x[0]] + pareto_costs_x.tolist() + [np.max(costs_x)],  # We add bounds
+        [np.max(costs_y)] + pareto_costs_y.tolist() + [np.min(pareto_costs_y)],  # We add bounds
+        where="post",
+        linestyle=":",
+    )
+
+    plt.title("Pareto-Front")
+    plt.xlabel(smac.scenario.objectives[0])
+    plt.ylabel(smac.scenario.objectives[1])
+    plt.legend()
+    plt.show()
+
 def main():
-    
+    objectives = ["loss", "time"]
     cs = create_configspace()
     facades: list[AbstractFacade] = []
     scenario = Scenario(
             cs,
+            objectives=objectives,
             trial_walltime_limit=3000,  # After 60 seconds, we stop the hyperparameter optimization
             n_trials=50,  # Evaluate max 500 different trials
             min_budget=1,  # Train the MLP using a hyperparameter configuration for at least 5 epochs
@@ -242,27 +287,30 @@ def main():
     # We want to run five random configurations before starting the optimization.
     initial_design = MFFacade.get_initial_design(scenario, n_configs=5)
 
+    multi_objective_algorithm = ParEGO(scenario)
+
     smac = MFFacade(
             scenario,
             train_model,
             initial_design=initial_design,
+            multi_objective_algorithm=multi_objective_algorithm,
             intensifier=intensifier,
             overwrite=True,
         )
-    incumbent = smac.optimize()
+    incumbents = smac.optimize()
 
     # Get cost of default configuration
     default_cost = smac.validate(cs.get_default_configuration())
-    print(f"Default cost ({intensifier.__class__.__name__}): {default_cost}")
+    print(f"Validated costs from default config: \n--- {default_cost}\n")
 
     # Let's calculate the cost of the incumbent
-    incumbent_cost = smac.validate(incumbent)
-    print(f"Incumbent cost ({intensifier.__class__.__name__}): {incumbent_cost}")
+    print("Validated costs from the Pareto front (incumbents):")
+    for incumbent in incumbents:
+        cost = smac.validate(incumbent)
+        print("---", cost)
 
-    facades.append(smac)
-    
-    print(incumbent, smac.validate)
-    plot_trajectory(facades)
+    # Let's plot a pareto front
+    plot_pareto(smac, incumbents)
 
 if __name__ == "__main__":
     main()
