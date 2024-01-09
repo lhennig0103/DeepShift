a44b8b43257e4da6ca6fd1b05ffff1c1a812c79f
Leona Hennig <l.hennig@ai.uni-hannover.de>
2024-01-09 11:00:14 +0100

    bash updated

--- dsnn_smac_bash1.sh
+++ dsnn_smac_bash1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -t 12:00:00
+#SBATCH -t 4:00:00
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -J "dsnn_smac_1"
@@ -9,4 +9,4 @@
 source /opt/software/pc2/EB-SW/software/Anaconda3/2022.05/etc/profile.d/conda.sh
 conda activate dsnn_smac
 
-python /scratch/hpc-prf-intexml/leonahennig/DeepShift/pytorch/smac_dsnn_mf.py
+python /scratch/hpc-prf-intexml/leonahennig/DeepShift/pytorch/smac_dsnn_mf_short.py
